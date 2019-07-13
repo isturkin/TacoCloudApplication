396afb2e9d05de0125241b08180402dfb2ea6503
Ivan Turkin <ivanturkinrussia@gmail.com>
2019-07-13 16:05:18 +0300

    Chapter 2: Web configuration was added. Short HomeController definition was moved to it.

--- src/test/java/com/ivanturkin/cloud/app/taco/HomeControllerTest.java
+++ src/test/java/com/ivanturkin/cloud/app/taco/HomeControllerTest.java
@@ -1,6 +1,5 @@
 package com.ivanturkin.cloud.app.taco;
 
-import com.ivanturkin.cloud.app.taco.controller.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
     @Autowired
