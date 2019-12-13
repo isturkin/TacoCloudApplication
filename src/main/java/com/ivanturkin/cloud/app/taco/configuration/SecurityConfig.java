c59bef14ec7057d7ce2bdcde2e22298c52816baa
Ivan Turkin <Ivan_Turkin@epam.com>
2019-12-13 15:34:49 +0300

    Security config with an in-memory user store for an authentication was added

---
+++ src/main/java/com/ivanturkin/cloud/app/taco/configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.ivanturkin.cloud.app.taco.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+        .inMemoryAuthentication()
+          .withUser("buzz")
+            .password("infinity")
+            .authorities("ROLE_USER")
+          .and()
+          .withUser("woody")
+            .password("bullseye")
+            .authorities("ROLE_USER");
+  }
+}
