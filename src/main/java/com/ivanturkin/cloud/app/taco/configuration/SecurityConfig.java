665ecea0e65c14fa7d8bf5eea43a6c197cde81f4
Ivan Turkin <Ivan_Turkin@epam.com>
2019-12-13 16:46:27 +0300

    BCrypt password encoder support was added

--- src/main/java/com/ivanturkin/cloud/app/taco/configuration/SecurityConfig.java
+++ src/main/java/com/ivanturkin/cloud/app/taco/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -13,6 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth
         .inMemoryAuthentication()
+        .passwordEncoder(new BCryptPasswordEncoder())
           .withUser("buzz")
             .password("infinity")
             .authorities("ROLE_USER")
