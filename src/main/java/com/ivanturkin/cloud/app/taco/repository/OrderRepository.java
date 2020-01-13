0d19bfcaf20f669ce34a6a0068d07bfeac653112
Ivan Turkin <Ivan_Turkin@epam.com>
2020-01-13 18:45:13 +0300

    Small fix

--- src/main/java/com/ivanturkin/cloud/app/taco/repository/OrderRepository.java
+++ src/main/java/com/ivanturkin/cloud/app/taco/repository/OrderRepository.java
@@ -4,9 +4,9 @@ import com.ivanturkin.cloud.app.taco.domain.Order;
 import com.ivanturkin.cloud.app.taco.domain.User;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
 
     List<Order> findByUserOrderByPlacedDateDesc(User user, Pageable pageable);
 }
